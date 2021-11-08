d5624425362ebfb53b75306c1b80de4aff900330
Xingsheng Wei <xw2815@columbia.edu>
2021-11-07 20:28:14 -0500

    start

---
+++ LarvaBot.py
@@ -0,0 +1,41 @@
+from lx16a import *
+from math import sin, cos
+import time
+
+print('Running')
+# This is the port that the controller board is connected to
+# This will be different for different computers
+# On Windows, try the ports COM1, COM2, COM3, etc...
+# On Raspbian, try each port in /dev/
+LX16A.initialize("/dev/ttyUSB0")
+
+# There should two servos connected, with IDs 1 and 2
+servo10 = LX16A(10)
+servo11 = LX16A(11)
+servo12 = LX16A(12)
+servo13 = LX16A(13)
+
+t = 0
+
+#def initialPos():
+#	pos11 = servo11.moveTimeRead()
+
+
+#def limit():
+
+while True:
+	# Two sine waves out of phase
+	# The servos can rotate between 0 and 240 degrees,
+	# So we adjust the waves to be in that range
+	servo10.moveTimeWrite(0)#0 is loose, 120 is tight
+	pos10=servo10.moveTimeRead()
+	servo11.moveTimeWrite(240) #240 is loose
+	pos11=servo11.moveTimeRead()
+	servo12.moveTimeWrite(0) #0 is loose
+	pos12=servo12.moveTimeRead()
+	#servo13.moveTimeWrite(120) #min 75, max 165, neutral 120,+-45
+	print(pos11)
+	print(pos12)
+	print('running')
+	
+	t += 0.02
---
+++ helloWorld.py
@@ -0,0 +1,27 @@
+from lx16a import *
+from math import sin, cos
+import time
+
+# This is the port that the controller board is connected to
+# This will be different for different computers
+# On Windows, try the ports COM1, COM2, COM3, etc...
+# On Raspbian, try each port in /dev/
+LX16A.initialize("/dev/ttyUSB0")
+
+# There should two servos connected, with IDs 1 and 2
+servo1 = LX16A(11)
+servo2 = LX16A(12)
+servo3 = LX16A(13)
+
+t = 0
+
+while True:
+	# Two sine waves out of phase
+	# The servos can rotate between 0 and 240 degrees,
+	# So we adjust the waves to be in that range
+	servo1.moveTimeWrite(240)
+	servo2.moveTimeWrite(240)
+	servo3.moveTimeWrite(120+30*sin(t)) #min 75, max 165, neutral 120,+-45
+	print(t)
+	
+	t += 0.010

8784d09c36dc3a69ec24dfc32bae87cd78b46922
Xingsheng Wei <xw2815@columbia.edu>
2021-11-10 09:22:46 -0500

    read initial position, initialize LED

--- LarvaBot.py
+++ LarvaBot.py
@@ -1,20 +1,57 @@
 from lx16a import *
 from math import sin, cos
 import time
+import RPi.GPIO as GPIO
+import numpy as np
 
-print('Running')
-# This is the port that the controller board is connected to
-# This will be different for different computers
-# On Windows, try the ports COM1, COM2, COM3, etc...
+print('Initializing...')
+# initializing LED
+red = 18
+green = 23
+blue = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(red,GPIO.OUT)
+GPIO.setup(green,GPIO.OUT)
+GPIO.setup(blue,GPIO.OUT)
+# LED show red
+GPIO.output(red,GPIO.HIGH)
+print('Initializing Done')
 # On Raspbian, try each port in /dev/
 #LX16A.initialize("/dev/ttyUSB0")
 LX16A.initialize("/dev/ttyUSB0")
-# There should two servos connected, with IDs 1 and 2
-servo10 = LX16A(10)
+# Initializing Servo
+#servo10 = LX16A(10)
 #servo11 = LX16A(11)
 #servo12 = LX16A(12)
 #servo13 = LX16A(13)
-servo20 = LX16A(20)
+#servo20 = LX16A(20)
+#servo21 = LX16A(21)
+#servo22 = LX16A(22)
+#servo23 = LX16A(23)
+servo = [LX16A(10),LX16A(11),LX16A(12),LX16A(13),LX16A(20),LX16A(21),LX16A(22),LX16A(23)]
+#initializing parameters
+nMotor = 8
+#homePos = 
+# LED show yellow for 1 sec
+GPIO.output(red,GPIO.HIGH)
+GPIO.output(green,GPIO.HIGH)
+print('Initializing Done')
+#initializing done
+
+#Auto Checking 
+
+#Auto Homing
+print('Read Initial Position')
+pos = []
+for i in range(0,nMotor):
+	pos.append(servo[i].moveTimeRead())
+print('Initial Position:');print(pos)
+#while
+
+
+
+
 t = 0
 
 #def initialPos():

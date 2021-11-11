407545981e03d22b3cb93399aa477b05e73aece1
LarvaBot Raspberry <xw2815@columbia.edu>
2021-11-11 17:08:46 -0500

    local

--- LarvaBot.py
+++ LarvaBot.py
@@ -14,22 +14,27 @@ GPIO.setwarnings(False)
 GPIO.setup(red,GPIO.OUT)
 GPIO.setup(green,GPIO.OUT)
 GPIO.setup(blue,GPIO.OUT)
+GPIO.output(red,GPIO.LOW)
+GPIO.output(green,GPIO.LOW)
+GPIO.output(blue,GPIO.LOW)
 # LED show red
 GPIO.output(red,GPIO.HIGH)
-print('Initializing Done')
+print('LED Initialized')
 # On Raspbian, try each port in /dev/
 #LX16A.initialize("/dev/ttyUSB0")
 LX16A.initialize("/dev/ttyUSB0")
+print('Motor Connected')
 # Initializing Servo
 #servo10 = LX16A(10)
-#servo11 = LX16A(11)
+servo11 = LX16A(11)
 #servo12 = LX16A(12)
 #servo13 = LX16A(13)
 #servo20 = LX16A(20)
 #servo21 = LX16A(21)
 #servo22 = LX16A(22)
 #servo23 = LX16A(23)
-servo = [LX16A(10),LX16A(11),LX16A(12),LX16A(13),LX16A(20),LX16A(21),LX16A(22),LX16A(23)]
+#servo = [LX16A(10),LX16A(11),LX16A(12),LX16A(13),LX16A(20),LX16A(21),LX16A(22),LX16A(23)]
+print('Motor Initialized')
 #initializing parameters
 nMotor = 8
 #homePos = 
@@ -64,17 +69,17 @@ while True:
 	# Two sine waves out of phase
 	# The servos can rotate between 0 and 240 degrees,
 	# So we adjust the waves to be in that range
-	servo10.moveTimeWrite(0)#0 is loose, 120 is tight
-	pos10=servo10.moveTimeRead()
+	#servo10.moveTimeWrite(0)#0 is loose, 120 is tight
+	#pos10=servo10.moveTimeRead()
 	#servo11.moveTimeWrite(240) #240 is loose
 	#pos11=servo11.moveTimeRead()
 	#servo12.moveTimeWrite(0) #0 is loose
 	#pos12=servo12.moveTimeRead()
 	#servo13.moveTimeWrite(120) #min 75, max 165, neutral 120,+-45
-	servo20.moveTimeWrite(0)#0 is loose, 120 is tight
-	pos20=servo20.moveTimeRead()
-	print(pos10)
-	print(pos20)
+	#servo20.moveTimeWrite(0)#0 is loose, 120 is tight
+	#pos20=servo20.moveTimeRead()
+	#print(pos10)
+	#print(pos20)
 	print('running')
 	
 	t += 0.01
--- helloWorld.py
+++ helloWorld.py
@@ -9,19 +9,19 @@ import time
 LX16A.initialize("/dev/ttyUSB0")
 
 # There should two servos connected, with IDs 1 and 2
-servo1 = LX16A(11)
-servo2 = LX16A(12)
-servo3 = LX16A(13)
-
+servo1 = LX16A(21)
+servo2 = LX16A(22)
+servo3 = LX16A(23)
+print('s')
 t = 0
 
 while True:
 	# Two sine waves out of phase
 	# The servos can rotate between 0 and 240 degrees,
 	# So we adjust the waves to be in that range
-	servo1.moveTimeWrite(240)
-	servo2.moveTimeWrite(240)
-	servo3.moveTimeWrite(120+30*sin(t)) #min 75, max 165, neutral 120,+-45
+	#servo1.moveTimeWrite(240)
+	#servo2.moveTimeWrite(240)
+	#servo3.moveTimeWrite(120+30*sin(t)) #min 75, max 165, neutral 120,+-45
 	print(t)
 	
 	t += 0.010
